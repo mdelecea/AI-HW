16495b424533a68e2f7c923245d3036f928b67b7
Marc de Lecea <laserno2@gmail.com>
2021-10-14 16:39:54 -0400

    renamed hw1

--- h1.py
+++ h1.py
@@ -41,25 +41,39 @@ class pancake_stack:
 
     def flip(self,index):
         '''reverse list from 0 to index'''
-        #cost = sum(self.stack[:index].size)
-        self.stack[:index] = self.stack[:index][::-1]
+        selfcopy = copy.deepcopy(self)
+        selfcopy.stack[:index] = selfcopy.stack[:index][::-1]
         for i in range(0,index):
-            if self.stack[i].orientation == 'w':
-                self.stack[i].orientation = 'b'
+            if selfcopy.stack[i].orientation == 'w':
+                selfcopy.stack[i].orientation = 'b'
             else:
-                self.stack[i].orientation = 'w'
+                selfcopy.stack[i].orientation = 'w'
+        return selfcopy
+
+    def expandNode(self):
+        '''expands a node by adding all possible nodes to stack'''
+        subfringes = []
+        for i in range(1,len(self.stack)+1):
+            newstack = copy.deepcopy(self).flip(i)
+            newstack.fliploc = i
+            newstack.parent = self
+            newstack.settotalcost()
+            subfringes.append(newstack)
+        return subfringes
     
     def pancake_heuristic(self):
         '''heuristic for pancake sorting'''
-        heuristic = 0
+        self.heuristic = 0
         for i in range(len(self.stack)):
             if self.stack[i].size-1 != i:
-                heuristic = max(heuristic,self.stack[i].size)
+                self.heuristic = max(self.heuristic,self.stack[i].size)
 
     def __str__(self):
         s = ""
         for i in range(0,len(self.stack)):
             s += str(self.stack[i].size) + self.stack[i].orientation
+            if i == self.fliploc-1:
+                s+="|"
         return s
 
     def __eq__(self,other):
@@ -79,45 +93,31 @@ class pancake_stack:
                 return False
         return True
 
+    def printresult(result):
+        '''prints the result of the pancake sorting algorithm'''
+        try:
+            result.parent.printresult()
+            print("")
+        except AttributeError:
+            pass
+        print(str(result))
+
 def pancake_sort(fringe,algo):
     if algo == "a":
         return pancake_sort_a(fringe)
     elif algo == "b":
         return pancake_sort_b(fringe)
 
-def tiebreaker(node1,node2):
+def tiebreaker(nodelist):
     '''tiebreaker for a* algorithm'''
-    node1num = ""
-    for i in range(0,len(node1.stack)):
-        node1num.append(str(node1.stack[i].size))
-        node1num.append("0" if node1.stack[i].orientation == 'b' else "1")
-    node2num = ""
-    for i in range(0,len(node2.stack)):
-        node2num.append(str(node2.stack[i].size))
-        node2num.append("0" if node2.stack[i].orientation == 'b' else "1")
-    return True if int(node1num)>int(node2num) else False
+    nodenum = [""]*len(nodelist)
+    for i in range(0,len(nodelist)):
+        for j in range(0,len(nodelist[i].stack)):
+            nodenum[i].append(str(nodelist[i].stack[j].size))
+            nodenum[i].append("0" if nodelist[i].stack[j].orientation == 'b' else "1")
+    mymax = max(map(len,int(nodenum)))
+    return [a for a in nodenum if len(a)==mymax]
 
-def expandNode(node):
-    '''expands a node by adding all possible nodes to stack'''
-    subfringe = []
-    for i in range(1,len(node.stack)+1):
-        node.parent = copy.deepcopy(node)
-        node.fliploc = i
-        node.fwd_cost += i
-        newnode = copy.deepcopy(node)
-        newnode.flip(i)
-        subfringe.append(newnode)
-    return subfringe
-
-def printresult(result):
-    '''prints the result of the pancake sorting algorithm'''
-    for i in range(0,len(result.stack)):
-        #print("Pancake",i,"size",result.stack[i].size-1,"orientation",result.stack[i].orientation)
-        print(result.stack[i].size,result.stack[i].orientation,sep="",end="")
-        if i == result.fliploc-1:
-            print("|",end="")
-    print("")
-    
 def pancake_sort_a(fringe):
     '''a* sorting algorithm for pancake flipping'''
     closed_set = []
@@ -126,13 +126,13 @@ def pancake_sort_a(fringe):
         for node in fringe:
             node.pancake_heuristic()
         minnode = min(fringe,key=lambda x: x.total_cost)
-        subfringe = expandNode(copy.deepcopy(minnode))
+        subfringe = copy.deepcopy(minnode).expandNode()
         closed_set.append(minnode)
         fringe.remove(minnode)
         for node in subfringe:
             if node not in closed_set:
                 fringe.append(node)
-            printresult(node)
+            node.printresult()
         print("")
     return minnode
 
@@ -142,24 +142,21 @@ def pancake_sort_b(fringe):
     while True:
         if fringe[0].goalCheck():
                 return fringe[0]
-        subfringe = expandNode(copy.deepcopy(fringe[0]))
+        subfringe = copy.deepcopy(fringe[0]).expandNode()
         closed_set.append(fringe.pop(0))
         for node in subfringe:
             if node not in closed_set:
                 fringe.append(node)
             if node.goalCheck():
                 return node
-            printresult(node)
+            node.printresult()
         print("")
-
-
 
 if __name__ == "__main__":
     #stackstr,algo = pancakeparams()
     stackstr = "2b1b3w4w"
-    algo = "a"
+    algo = "b"
     node = pancake_stack(stackstr)
     fringe = [node]
     result = pancake_sort(fringe,algo)
-    printresult(result)
-
+    result.printresult()
